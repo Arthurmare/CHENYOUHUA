09d6d0bdb1546fe32437b068756a74dec3a16798
student <a048889276@gmail.com>
2023-05-31 10:43:11 +0800

    The Second part

--- app/src/main/java/com/example/chenyouhua/MainActivity.kt
+++ app/src/main/java/com/example/chenyouhua/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.chenyouhua.ui.theme.CHENYOUHUATheme
 import kotlin.reflect.typeOf
 
