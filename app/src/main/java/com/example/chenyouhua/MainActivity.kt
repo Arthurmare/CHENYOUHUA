233460000148ee0e18d79b9d1a8b317708f60fac
student <a048889276@gmail.com>
2023-05-31 11:45:34 +0800

    The third part

--- app/src/main/java/com/example/chenyouhua/MainActivity.kt
+++ app/src/main/java/com/example/chenyouhua/MainActivity.kt
@@ -3,17 +3,19 @@ package com.example.chenyouhua
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.OffsetEffect
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.style.TextAlign
@@ -49,10 +51,19 @@ fun Greeting(name: String) {
 
 @Composable
 fun personal(name: String){
+
+
     Column (){
         Text(text = "作者: $name",textAlign = TextAlign.Center, modifier = Modifier.width(1000.dp))
         Image(painter = painterResource(id = R.drawable.map), contentDescription = "地圖")
-
+        Box(modifier = Modifier.fillMaxSize()){
+            Canvas(modifier = Modifier){
+                drawRect(Color.Blue, Offset(600f,150f), Size(40F,40F))
+            }
+            Canvas(modifier = Modifier){
+                drawRect(Color.Blue, Offset(1300f,700f), Size(40F,40F))
+            }
+        }
     }
 }
 
